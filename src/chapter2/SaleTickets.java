cdda758c15dd4d2ec8776d52e64893b8b870c4b5
suruomo <www.suruomo@hotmail.com>
2019-12-13 19:27:37 +0800

    卖票例子

---
+++ src/chapter2/SaleTickets.java
@@ -0,0 +1,32 @@
+package chapter2;
+
+
+class SaleTicket implements Runnable{
+    private int tickets = 20;
+    @Override
+    public void run(){
+        while(tickets > 0){
+            synchronized (SaleTicket.class){
+                if(tickets > 0){
+                    System.out.println(Thread.currentThread().getName() + "卖出 第 "+ (tickets--)+"张票");
+                    try{
+                        Thread.sleep(1000);
+                    }catch(InterruptedException e){
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+}
+public class SaleTickets {
+    public static void main(String[] args){
+        SaleTicket st = new SaleTicket();
+        Thread t1 = new Thread(st, "一号窗口");
+        Thread t2 = new Thread(st, "二号窗口");
+        Thread t3 = new Thread(st, "三号窗口");
+        t1.start();
+        t2.start();
+        t3.start();
+    }
+}
