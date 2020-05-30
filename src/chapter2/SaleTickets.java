c3a6275fd1800e389e4928208bd0c7b74b8ba859
suruomo <www.suruomo@hotmail.com>
2020-05-30 10:35:50 +0800

    修改卖票窗口

--- src/chapter2/SaleTickets.java
+++ src/chapter2/SaleTickets.java
@@ -1,25 +1,30 @@
 package chapter2;
 
 
-class SaleTicket implements Runnable{
-    private int tickets = 20;
-    @Override
-    public void run(){
-        while(tickets > 0){
-            synchronized (SaleTicket.class){
-                if(tickets > 0){
-                    System.out.println(Thread.currentThread().getName() + "卖出 第 "+ (tickets--)+"张票");
-                    try{
-                        Thread.sleep(1000);
-                    }catch(InterruptedException e){
-                        e.printStackTrace();
+
+/**
+ * 卖票
+ * @author 苏若墨
+ */
+public class SaleTickets {
+    static class SaleTicket implements Runnable{
+         static int tickets = 20;
+        @Override
+        public void run(){
+            while(tickets > 0){
+                synchronized (SaleTicket.class){
+                    if(tickets > 0){
+                        System.out.println(Thread.currentThread().getName() + "卖出 第 "+ (tickets--)+"张票");
+                        try{
+                            Thread.sleep(1000);
+                        }catch(InterruptedException e){
+                            e.printStackTrace();
+                        }
                     }
                 }
             }
         }
     }
-}
-public class SaleTickets {
     public static void main(String[] args){
         SaleTicket st = new SaleTicket();
         Thread t1 = new Thread(st, "一号窗口");
