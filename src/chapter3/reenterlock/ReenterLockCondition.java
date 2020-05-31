27d947f26d7ab54fc73bd5d6c0bd972cbe17cdc8
suruomo <www.suruomo@hotmail.com>
2020-05-31 09:38:08 +0800

    Condition

---
+++ src/chapter3/reenterlock/ReenterLockCondition.java
@@ -0,0 +1,42 @@
+package chapter3.reenterlock;
+
+import org.omg.PortableServer.THREAD_POLICY_ID;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * 搭配ReentrantLock，线程等待和线程唤醒
+ * @author 苏若墨
+ */
+public class ReenterLockCondition implements Runnable{
+    static ReentrantLock reentrantLock=new ReentrantLock();
+    static Condition condition=  reentrantLock.newCondition();
+
+
+    @Override
+    public void run() {
+        reentrantLock.lock();
+        try{
+            condition.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }finally {
+            reentrantLock.unlock();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        ReenterLockCondition reenterLockCondition=new ReenterLockCondition();
+        Thread thread=new Thread(reenterLockCondition);
+        thread.start();
+        Thread.sleep(1000);
+//        先获得锁才能唤醒
+        reentrantLock.lock();
+        try{
+            condition.signal();
+        } finally {
+            reentrantLock.unlock();
+        }
+    }
+}
