2784354a2374d45c86f59ee2e9ebd0a18bebb1c7
suruomo <www.suruomo@hotmail.com>
2020-05-31 09:23:34 +0800

    公平锁

---
+++ src/chapter3/reenterlock/FairLock.java
@@ -0,0 +1,31 @@
+package chapter3.reenterlock;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * 公平锁，按顺序，效率低下，维护成本大
+ * @author 苏若墨
+ */
+public class FairLock implements Runnable{
+    static ReentrantLock reentrantLock=new ReentrantLock();
+
+    @Override
+    public void run() {
+        while (true){
+            reentrantLock.lock();
+            try {
+                System.out.println(Thread.currentThread().getName()+"获得锁");
+            }finally {
+                reentrantLock.unlock();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        FairLock lock=new FairLock();
+        Thread t1=new Thread(lock);
+        Thread t2=new Thread(lock);
+        t1.start();
+        t2.start();
+    }
+}
